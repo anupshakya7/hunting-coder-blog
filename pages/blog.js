45c0e8c7206390d46412934198f1cf04b2eef404
anupshakya7 <anupshk39@gmail.com>
2024-08-22 23:14:14 +0545

    Blog Post Single Page Created

--- pages/blog.js
+++ pages/blog.js
@@ -1,7 +1,27 @@
 import React from "react";
+import styles from "../styles/Blog.module.css";
+import Link from "next/link";
 
 const blog = () => {
-  return <div>This is blog page</div>;
+  return (
+    <div className={styles.latest_blogs}>
+      <h2 className={styles.blogTitle}>Latest Blogs</h2>
+      <div className={styles.blogItem}>
+        <Link href={"/blogpost/learn-javascript"}>
+          <h3>How to Learn JavaScript in 2022?</h3>
+        </Link>
+        <p>Javscript is the language used to design logic for the web</p>
+      </div>
+      <div className={styles.blogItem}>
+        <h3>How to Learn JavaScript in 2022?</h3>
+        <p>Javscript is the language used to design logic for the web</p>
+      </div>
+      <div className={styles.blogItem}>
+        <h3>How to Learn JavaScript in 2022?</h3>
+        <p>Javscript is the language used to design logic for the web</p>
+      </div>
+    </div>
+  );
 };
 
 export default blog;
--- pages/blogpost/[slug].js
+++ pages/blogpost/[slug].js
@@ -1,10 +1,27 @@
 import React from "react";
 import { useRouter } from "next/router";
+import styles from "../../styles/BlogPost.module.css";
 
 const slug = () => {
   const router = useRouter();
   const { slug } = router.query;
-  return <div>{slug}</div>;
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.title}>Title of the page {slug}</h1>
+      <div className={styles.description}>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Natus dolorum
+        esse facilis nemo dolorem nihil nulla quaerat amet, earum dolore
+        suscipit quis alias eius quod voluptas sint consectetur enim atque?
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam
+        aspernatur, voluptatibus nam placeat, unde doloremque eligendi explicabo
+        eveniet alias repellendus impedit, necessitatibus consectetur provident
+        iure at! Perspiciatis autem assumenda eum. Lorem, ipsum dolor sit amet
+        consectetur adipisicing elit. Accusantium, nisi? Consequuntur quia earum
+        magnam ipsa reprehenderit iste maxime eum impedit nihil debitis in modi
+        ab culpa, assumenda est et dolorem.
+      </div>
+    </div>
+  );
 };
 
 export default slug;
